ede1f741435ab2818c9db087c9cc6a0c96e1a813
Kjulam <kjulamotlkcm@outlook.com>
2025-08-08 20:40:54 +0800

    First commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+// use std::env;
+
+const VERSION: &str = "250808a";
+const AUTHOR: &str = "Kjulam";
+
+fn main() -> () {
+    println!("Brainfuck interpreter version {} by {}", VERSION, AUTHOR);
+}
